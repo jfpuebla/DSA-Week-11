833f01ba6501533399e823b67fb53f798ebcf337
jfpuebla <johnfrancispuebla21@gmail.com>
2021-09-11 19:11:08 +0800

    just editing something

--- index.java
+++ index.java
@@ -1,6 +1,10 @@
 public class index{
+    //Insert here the methods only for our class
+
+    //the main below is for test run to check our class
     public static void main(String[] args){
         System.out.println("nice wan team");
         System.out.println("Okay");
     } 
+
 }
