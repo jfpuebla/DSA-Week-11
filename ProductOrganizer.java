75dc9076538880566375fd1dd3b0116f78361991
jfpuebla <johnfrancispuebla21@gmail.com>
2021-09-17 18:59:31 +0800

    Setting  up the needed methods

--- ProductOrganizer.java
+++ ProductOrganizer.java
@@ -1,11 +1,57 @@
 public class ProductOrganizer{
     //Insert here the methods only for our class
 
+    //returns the user's product type (Category,price,name)
+    public static askInput(){
 
-    
-    //hey how are you im fine thank you
-    // hello hello -kareze
-    // hi - miguel
+    }
+//------------------------------------------------------------------------------------
+    //PRICE METHODS BELOW
+
+    //returns the sorted price in ASCENDING order
+    public static ascPriceSort(){
+
+    }
+
+    //returns the sorted price in DESCENDING order
+    public static desPriceSort(){
+
+    }
+//-------------------------------------------------------------------------------------
+    //returns the category in alphabetical order (A to Z NOT Z to A para speed code hihi)
+    public static sortCategory(){
+
+    }
+
+//-----------------------------------------------------------------------------------------
+    //ALPHABETICAL NAMES METHODS BELOW
+
+    //ascending of name sort from A to Z
+    public static ascNameSort(){
+
+    }
+
+    //descending of name sort from Z to A
+    public static desNameSort(){
+
+    }
+//-------------------------------------------------------------------------------------------
+    //deletes the product
+    public static deleteProduct(){
+
+    }
+    //adds the product
+    public static addProduct(){
+
+    }
+
+    //prints the needed things here
+    public static void printSomethingHere(){
+
+    }
+
+
+
 
     //algorithms insider here
     
--- mainRun.java
+++ mainRun.java
@@ -2,6 +2,6 @@ public class mainRun {
     public void main (String[] args){
         //Insert here the main flow of our program 
 
-        
+    }
 
 }
