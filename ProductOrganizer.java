f01ae9b27a55d95dbb931fff9d177019da00acd1
jfpuebla <johnfrancispuebla21@gmail.com>
2021-09-17 18:30:14 +0800

    Adding foundations

--- ProductOrganizer.java
+++ ProductOrganizer.java
@@ -1,11 +1,8 @@
 public class ProductOrganizer{
     //Insert here the methods only for our class
 
-    //the main below is for test run to check our class
-    // public static void main(String[] args){
-    //     System.out.println("nice wan team");
-    //     System.out.println("Okay");
-    // } 
+
+    
     //hey how are you im fine thank you
     // hello hello -kareze
     // hi - miguel
--- mainRun.java
+++ mainRun.java
@@ -1,8 +1,7 @@
 public class mainRun {
     public void main (String[] args){
         //Insert here the main flow of our program 
-        //call the class
-        //hi
-    }asfdasdfasdfasdf
-    // trial trial 
+
+        
+
 }
